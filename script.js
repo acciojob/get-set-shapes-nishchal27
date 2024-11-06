a19a412171e75fcddfe5bcca58f6b8c005054e6d
nishchal singh <singhnishchal27@gmail.com>
2024-11-06 21:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	// Getter for width
+	get width() {
+		return this._width;
+	}
+
+	// Getter for height
+	get height() {
+		return this._height;
+	}
+
+	// Method to calculate area
+	getArea() {
+		return this.width * this.height;
+	}
+}
+
+// Define the Square subclass
+class Square extends Rectangle {
+	constructor(side) {
+		// Call the Rectangle constructor with both width and height as side
+		super(side, side);
+	}
+
+	// Method to calculate perimeter
+	getPerimeter() {
+		return 4 * this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
